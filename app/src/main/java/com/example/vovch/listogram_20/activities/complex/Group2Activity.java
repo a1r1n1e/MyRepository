5e2ae55a9a8589ce40eb75dc6b901f9212841527
Vladimir Arnold <vovch2ara@gmail.com>
2018-02-09 02:09:02 +0300

    Version 0_94_7 Group2Activity Lists Form Kill List Toast Added

--- app/src/main/java/com/example/vovch/listogram_20/activities/complex/Group2Activity.java
+++ app/src/main/java/com/example/vovch/listogram_20/activities/complex/Group2Activity.java
@@ -273,7 +273,6 @@ public class Group2Activity extends WithLoginActivity {
             String message = "Want To Kill List?";
             String button1String = "Confirm";
             String button2String = "Cancel";
-            String nuller = null;
 
             AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
             builder.setMessage(message);
